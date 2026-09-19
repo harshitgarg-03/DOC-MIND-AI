429a0eb93236f5b18d6d266671b5a9f67f759a0e
Harshit Garg~ <gargh2120@gmail.com>
2026-09-19 20:39:26 +0530

    fix

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,9 +1,22 @@
 import { betterAuth } from "better-auth/minimal";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma"; // your prisma client instance
+import { jwt } from "better-auth/plugins";
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
-    }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+
+  plugins: [jwt()],
 });
